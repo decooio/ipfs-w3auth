0031b131f5e637c0a10e21fa86e98b15b53c8206
KayleCoder <kalecoder@outlook.com>
2024-10-12 10:14:34 +0800

    Fix proxy code support caddy auth
    Signed-off-by: KayleCoder <kalecoder@outlook.com>

--- src/index.ts
+++ src/index.ts
@@ -16,18 +16,12 @@ server.use(authHandler);
 const proxy = httpProxy.createProxyServer({});
 
 server.all('*', (req: Request, res: Response) => {
-  const target = process.env.IPFS_ENDPOINT || 'http://127.0.0.1:5001';
-  console.log(`Validation success. Proxying request to ${target}`);
-
-  proxy.web(req, res, {target}, error => {
-    console.error(error);
-    res.writeHead(500, {'Content-Type': 'application/json'});
-    res.end(
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(
       JSON.stringify({
-        Error: error.message,
+        Success: true,
       })
-    );
-  });
+  );
 });
 
 const port = process.env.PORT || 5050;
